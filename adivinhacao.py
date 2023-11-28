89da7872cc6f2c4083d2460f071270b175d2ef2e
Alexandre Beiruth <101944430+Alebeiruth@users.noreply.github.com>
2023-11-28 14:55:53 -0300

    Update adivinhacao.py

--- adivinhacao.py
+++ adivinhacao.py
@@ -21,7 +21,7 @@ def jogar():
         total_de_tentativas = 5
 
     for rodada in range(1, total_de_tentativas + 1):
-        print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+        print(f"Tentativa {rodada} de {total_de_tentativas}")
         chute_str = input("Digite um número entre 1 e 100: ")
         print("Você digitou", chute_str)
         chute = int(chute_str)
